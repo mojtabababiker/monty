c5dc787490a1de306a323e21a936e01a89600056
mojtabababiker <mojmohammad98@gmail.com>
2023-08-23 22:02:33 -0700

    Adding the monty.h header file file, branch mainfunction project monty

--- monty.h
+++ monty.h
@@ -1,6 +1,13 @@
 #ifndef MONTY_H
 #define MONTY_H
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <unistd.h>
 
+
+extern stack_t *stack_head;
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -18,6 +25,7 @@ typedef struct stack_s
         struct stack_s *next;
 } stack_t;
 
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -31,5 +39,33 @@ typedef struct instruction_s
         char *opcode;
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
+
+/* helper functions */
+int parseFile(char *fileName); /*in the scanning_proccess.c file*/
+char *_strtok(char *str, char delim, size_t *len); /*in the _strtok.c*/
+int isValide(char *opcode); /*in the validation.c*/
+int exec_instruction(char *opcode, char *args, unsigned int line_num);
+int freeAll(void); /*main.c*/
+
+/* stack operations functions */
+stack_t *stack_push(stack_t **stack_head, int num);
+int stack_pop(stack_t **stack_head, int *err);
+int stack_top(stack_t **stack_head, int *err);
+
+/* opcode functions */
+void push_op(__attribute__((unused))stack_t **stack,
+             __attribute__((unused))unsigned int line_number);
+void pall_op(stack_t **stack_head,
+	     __attribute__((unused))unsigned int line_number);
+void pint_op(stack_t **stack_head, unsigned int line_number);
+void pop_op(stack_t **stack_head, unsigned int line_number);
+void swap_op(stack_t **stack_head, unsigned int line_number);
+void add_op(stack_t **stack_head, unsigned int line_number);
+void sub_op(stack_t **stack_head, unsigned int line_number);
+void mul_op(stack_t **stack_head, unsigned int line_number);
+void div_op(stack_t **stack_head, unsigned int line_number);
+void mod_op(stack_t **stack_head, unsigned int line_number);
+void nop_op(__attribute__((unused))stack_t **stack_head,
+            __attribute__((unused))unsigned int line_number);
 
 #endif /** MONTY_H */
